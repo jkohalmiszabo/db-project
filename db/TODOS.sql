6b55807944e90f6e08120564eb4bbd2979367c7d
jkohalmiszabo <julia.koehalmi@mng.ch>
2026-01-06 08:30:54 +0100

    fd

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -11,27 +11,29 @@ CREATE TABLE todos (
     due DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
-CREATE TABLE zuständiger Arzt (
+CREATE TABLE zustaendigerArzt (
     Arzt-ID INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(250) NOT NULL,
+    Vorname VARCHAR(250) NOT NULL
+    Nachname VARCHAR(250) NOT NULL,
     Spital VARCHAR(250),
     Telefonnummer INT(10)
 );
 
-CREATE TABLE Patient/Empfänger (
+CREATE TABLE PatientEmpfänger (
     Patienten-ID INT AUTO_INCREMENT PRIMARY KEY,
     Telefonnummer INT(10) NOT NULL UNIQUE,
     Spital VARCHAR(250) NOT NULL,
-    Name VARCHAR(250) NOT NULL,
+    Vorname VARCHAR(250) NOT NULL,
+    Nachname VARCHAR(250) NOT NULL,
     Gewicht(kg) FLOAT(4) NOT NULL,
-    Körpergrösse(m) FLOAT(4) NOT NULL,
+    Koerpergroesse(m) FLOAT(4) NOT NULL,
     Blutgruppe VARCHAR(4) NOT NULL,
     Alterskategorie INT(2) NOT NUll,
     Alter INT(3) NOT NULL
     FOREIGN KEY (Arzt-ID) REFERENCES zuständiger Arzt(Arzt-ID)
 );
 
-CREATE TABLE krankes Organ (
+CREATE TABLE krankesOrgan (
     FOREIGN KEY (Patienten-ID) REFERENCES Patient/Empfänger(Patienten-ID),
     ORGAN VARCHAR(30) NOT NULL,
     Dringlichkeit INT(2) NOT NULL
@@ -42,13 +44,20 @@ CREATE TABLE Spenderorgane (
     FOREIGN KEY (Verstorbenen-ID) REFERENCES Verstorbener/Hirntoter (Verstorbenen-ID)
 );
 
-CREATE TABLE Verstorbener/Hirntoter (
+CREATE TABLE VerstorbenerHirntoter (
     Verstorbenen-ID INT AUTO-INCREMENT PRIMARY KEY,
     Telefonnummer eines Angehörigen INT(10),
     Spital VARCHAR (250),
-    Name VARCHAR (250),
+    Vorname VARCHAR(250),
+    Nachname VARCHAR(250),
     Gewicht(kg) FLOAT(4) NOT NULL,
     Körpergrösse(m) FLOAT(4) NOT NULL,
     Blutgruppe VARCHAR(4) NOT NULL,
     Alterskategorie INT(2) NOT NUll
 );
+
+INSERT INTO zustaendigerArzt(Vorname,Nachname,Spital,Telefonnummer) VALUES
+('Joseph', 'Müller',' Uniklinik Zürich','0791234567'),
+('Anna','Schneider',' Uniklinik Luzern','0791234568');
+
+INSERT INTO PatientEmpfänger (Telefonnummer,Spital,Vorname,Nachname,Gewicht(kg),Koerpergroesse)
