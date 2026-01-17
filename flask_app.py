9652ddda5400a5ef1b54cea0b330576ea29c6084
jkohalmiszabo <julia.koehalmi@mng.ch>
2026-01-17 20:11:38 +0100

    k

--- flask_app.py
+++ flask_app.py
@@ -463,8 +463,8 @@ def index():
 @login_required
 @role_required("admin")
 def users():
-    users_list = db_read("SELECT username FROM users ORDER BY username", ())
-    return render_template("users.html", users=users_list)
+    return "USERS ROUTE OK ✅", 200
+
 
 
 @app.route("/dbexplorer", methods=["GET", "POST"])
