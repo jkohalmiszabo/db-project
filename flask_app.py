e226bf0bf63cbbb38f25c42e57b5bfb05f067bef
jkohalmiszabo <julia.koehalmi@mng.ch>
2026-01-17 17:24:01 +0100

    k

--- flask_app.py
+++ flask_app.py
@@ -294,7 +294,7 @@ def new_deceased():
     for organ in organs:
         db_write("INSERT INTO spenderorgane (verstorbenenid, organ) VALUES (%s,%s)", (vid, organ))
 
-    rreturn redirect(url_for("allocate", run=1))
+    return redirect(url_for("allocate", run=1))
 
 
 
